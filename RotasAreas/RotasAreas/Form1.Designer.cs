4271bd1c726553ad91234f005ff0ccca4159e5ac
IGOR MEDEIROS PEDRO <u15259@academico.cotuca.unicamp.br>
2017-10-27 17:08:36 -0200

    Primeiro

---
+++ RotasAreas/RotasAreas/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RotasAreas
+{
+    partial class frmRotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRotas));
+            this.imgRotasAereas = new System.Windows.Forms.ImageList(this.components);
+            this.dlgNomesCidades = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // imgRotasAereas
+            // 
+            this.imgRotasAereas.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgRotasAereas.ImageStream")));
+            this.imgRotasAereas.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgRotasAereas.Images.SetKeyName(0, "Mapa Marte sem rotas.jpg");
+            // 
+            // dlgNomesCidades
+            // 
+            this.dlgNomesCidades.FileName = "openFileDialog1";
+            this.dlgNomesCidades.FileOk += new System.ComponentModel.CancelEventHandler(this.dlgNomesCidades_FileOk);
+            // 
+            // frmRotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "frmRotas";
+            this.Text = "Rotas Aéreas entre cidades";
+            this.Load += new System.EventHandler(this.frmRotas_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imgRotasAereas;
+        private System.Windows.Forms.OpenFileDialog dlgNomesCidades;
+    }
+}
+
---
+++ RotasAreas/RotasAreas/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RotasAreas
+{
+    public partial class frmRotas : Form
+    {
+        public frmRotas()
+        {
+            InitializeComponent();
+        }
+
+        private void dlgNomesCidades_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void Leitura()
+        {
+            StreamReader arq = new StreamReader("C:\\Users\\u15259\\Desktop\\RotasAreas\\CaminhosEntreCidadesMarte.txt");
+            while (!arq.EndOfStream)
+            {
+                String linha;
+                linha = arq.ReadLine();
+            }
+        }s
+
+        private void frmRotas_Load(object sender, EventArgs e)
+        {
+            Leitura();
+            this.BackgroundImage = imgRotasAereas.Images[0];
+        }
+    }
+}
